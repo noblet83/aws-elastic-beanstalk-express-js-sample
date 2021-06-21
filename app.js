f468caa14ebac88bf3211c31d9feaa27f5fec01d
Tom Noble <tnnobl@amazon.com>
2021-06-21 16:13:14 -0400

    another'

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Does My Pipeline STILL Works again!'));
+app.get('/', (req, res) => res.send('Is My Pipeline STILL Works again!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
